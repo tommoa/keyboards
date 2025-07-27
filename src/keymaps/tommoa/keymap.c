0d6d6317cab00b0b87582c81f0194c4e4fc5334e
Tom Almeida <tom@tommoa.me>
2025-07-28 01:44:28 +1000

    fix: Ensure that KC_6 is used for the '6' key in the _LOWER layer

--- src/keymaps/tommoa/keymap.c
+++ src/keymaps/tommoa/keymap.c
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid(
   KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_DOT,  KC_7,      KC_8,    KC_9,    XXXXXXX, KC_F12,
-  KC_DEL,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_4,      KC_5,    KC_7,    XXXXXXX, KC_BSLS,
+  KC_DEL,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_4,      KC_5,    KC_6,    XXXXXXX, KC_BSLS,
   _______, KC_BSLS, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_EQL,  KC_1,      KC_2,    KC_3,    KC_EQL,  KC_0,
   _______, _______, _______, _______, _______, _______, _______, KC_0,      KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
