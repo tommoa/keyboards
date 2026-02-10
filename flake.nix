819c8a801ee8145da5486a05cdb062796c916149
Tom Almeida <tom@tommoa.me>
2026-02-10 17:34:40 +1100

    feat(zmk/preonic): add ZMK firmware with Nix build
    Convert the Preonic QMK keymap to ZMK, targeting the upstream board
    definition at zmk/app/boards/olkb/preonic/. All six layers (QWERTY,
    Colemak, Gaming, Qwerty-in-Colemak, Lower, Raise) are translated with
    home-row mods using ZMK's balanced hold-tap flavor at 175ms, matching
    the QMK PERMISSIVE_HOLD behavior.
    
    Shared behavior definitions (hml/hmr hold-taps, layer indices) live in
    zmk/shared/base.keymap so additional boards can reuse them by including
    the file and defining their own layers.
    
    The Nix build uses lilyinstarlight/zmk-nix with a custom installPhase
    for the Preonic's STM32 DFU bootloader (produces
    .bin, not .uf2). Available outputs:
      - nix build .#zmk - nix run .#zmk-flash - nix run .#zmk-update

--- flake.nix
+++ flake.nix
@@ -1,36 +1,42 @@
 {
   description = "Tools to build and flash my custom keyboard";
 
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-      flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/master";
-      qmk-nix-utils = {
-        url = "github:tommoa/qmk-nix-utils";
-        inputs.nixpkgsUnstable.follows = "nixpkgs";
-      };
-      qmk-firmware-source = {
-        type = "git";
-        url = "https://github.com/qmk/qmk_firmware";
-        flake = false;
-        submodules = true;
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/master";
+    qmk-nix-utils = {
+      url = "github:tommoa/qmk-nix-utils";
+      inputs.nixpkgsUnstable.follows = "nixpkgs";
     };
+    qmk-firmware-source = {
+      type = "git";
+      url = "https://github.com/qmk/qmk_firmware";
+      flake = false;
+      submodules = true;
+    };
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils-plus
-    , qmk-nix-utils
-    , qmk-firmware-source
-    }: flake-utils-plus.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
+    {
+      self,
+      nixpkgs,
+      flake-utils-plus,
+      qmk-nix-utils,
+      qmk-firmware-source,
+      zmk-nix,
+    }:
+    flake-utils-plus.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-      qmk-utils-factory = builtins.getAttr system qmk-nix-utils.utils-factory;
+        qmk-utils-factory = builtins.getAttr system qmk-nix-utils.utils-factory;
 
-      preonic = qmk-utils-factory
-        {
+        preonic-qmk = qmk-utils-factory {
           inherit qmk-firmware-source;
           src = ./qmk/preonic;
           keyboard-name = "preonic";
@@ -41,15 +47,57 @@
             ${pkgs.dfu-util}/bin/dfu-util -a 0 -s 0x08000000:leave -w -D $BIN_FILE
           '';
         };
-    in
-    {
-      devShell = preonic.dev-shell;
-      defaultPackage = preonic.hex;
-      defaultApp = {
-        type = "app";
-        program = "${preonic.flasher}/bin/flasher";
-      };
-      apps.flash = preonic.flasher;
-    }
+
+        zmk-flasher = pkgs.writeShellScriptBin "zmk-flash" ''
+          set -euo pipefail
+          ${pkgs.dfu-util}/bin/dfu-util -a 0 -s 0x08000000:leave -w -D "${preonic-zmk}/zmk.bin"
+        '';
+
+        preonic-zmk = zmk-nix.legacyPackages.${system}.buildKeyboard {
+          name = "preonic-zmk";
+          src = nixpkgs.lib.sourceFilesBySuffices ./zmk [
+            ".conf"
+            ".keymap"
+            ".yml"
+          ];
+          board = "preonic";
+          config = "preonic/config";
+          zephyrDepsHash = "sha256-mUJpGWlU+rGbcWtKs/SuombCJ3RcIDMTiuMicwLX1D4=";
+          # The Preonic rev3 Drop uses STM32 DFU, not UF2 bootloader,
+          # so the build produces .bin/.hex instead of .uf2.
+          installPhase = ''
+            runHook preInstall
+            mkdir $out
+            cp zephyr/zmk.bin $out/
+            runHook postInstall
+          '';
+          meta = {
+            description = "ZMK firmware for Preonic rev3 Drop";
+            license = nixpkgs.lib.licenses.mit;
+            platforms = nixpkgs.lib.platforms.all;
+          };
+        };
+      in
+      {
+        devShells.default = preonic-qmk.dev-shell;
+        devShells.zmk = zmk-nix.devShells.${system}.default;
+
+        packages.default = preonic-qmk.hex;
+        packages.zmk = preonic-zmk;
+
+        apps.default = {
+          type = "app";
+          program = "${preonic-qmk.flasher}/bin/flasher";
+        };
+        apps.flash = preonic-qmk.flasher;
+        apps.zmk-flash = {
+          type = "app";
+          program = "${zmk-flasher}/bin/zmk-flash";
+        };
+        apps.zmk-update = {
+          type = "app";
+          program = "${zmk-nix.packages.${system}.update}/bin/update";
+        };
+      }
     );
 }
